8eb0b26644a7c7ab3b61abc8f5c611c48d17d006
Sangam Basnet <sangambasnet1800@gmail.com>
2024-12-22 20:55:44 +0545

    databse

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/db_Carinfo");
+        console.log("Mongoose Connected")
+
+    } catch (e) {
+        console.log("MongoDb not Connected");
+
+    }
+}
