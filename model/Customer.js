c80d8276084ffbf2ea5c1b76e57777f0d16c3c70
Sangam Basnet <sangambasnet1800@gmail.com>
2024-12-26 16:29:50 +0545

    Cutomer js initialization

---
+++ model/Customer.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const customerSchema = new mongoose.Schema({
+    full_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+})
+
+const Customer = mongoose.model("customers", customerSchema);
+module.exports = Customer;
