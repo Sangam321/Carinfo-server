f04beb426bc331d5cffd09a9dd57b6986e99bdbc
Sangam Basnet <sangambasnet1800@gmail.com>
2025-01-03 09:05:04 +0545

    modify customer controller to user controller

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,9 +1,9 @@
-const Customer = require('../model/Customer')
+const User = require('../model/User')
 const nodemailer = require("nodemailer")
 const findAll = async (requestAnimationFrame, res) => {
     try {
-        const customers = await Customer.find();
-        res.status(200).json(customers);
+        const users = await User.find();
+        res.status(200).json(users);
     } catch (e) {
         res.json(e)
     }
@@ -11,8 +11,8 @@ const findAll = async (requestAnimationFrame, res) => {
 
 const save = async (req, res) => {
     try {
-        const customer = new Customer(req.body);
-        await customer.save();
+        const user = new User(req.body);
+        await user.save();
 
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
@@ -27,12 +27,12 @@ const save = async (req, res) => {
 
         const info = await transporter.sendMail({
             from: "sangambasnett888@gmail.com",
-            to: customer.email,
-            subject: "Customer Registration",
+            to: user.email,
+            subject: "User Registration",
             html: `
                 <h1>
                     Your Registration has been Completed!
-                </h1><p> Your user id is ${customer.id}</p>
+                </h1><p> Your user id is ${user.id}</p>
                 `
 
 
@@ -42,7 +42,7 @@ const save = async (req, res) => {
 
 
 
-        res.status(201).json({ customer, info })
+        res.status(201).json({ user, info })
     } catch (e) {
         res.json(e)
     }
@@ -50,8 +50,8 @@ const save = async (req, res) => {
 
 const findById = async (req, res) => {
     try {
-        const customer = await Customer.findById(req.params.id);
-        res.status(200).json(customer)
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user)
     } catch (e) {
         res.json(e)
     }
@@ -60,7 +60,7 @@ const findById = async (req, res) => {
 }
 const deleteById = async (req, res) => {
     try {
-        const customer = await Customer.findByIdAndDelete(req.params.id);
+        const user = await User.findByIdAndDelete(req.params.id);
         res.status(200).json("data Deleted")
     } catch (e) {
         res.json(e)
@@ -70,8 +70,8 @@ const deleteById = async (req, res) => {
 }
 const update = async (req, res) => {
     try {
-        const customer = await Customer.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.status(201).json(customer)
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.status(201).json(user)
     } catch (e) {
         res.json(e)
     }
